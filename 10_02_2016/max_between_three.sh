af2b5af9c2837750aea5b1a80b8eb259a10df343
techpool <suryadeep10@gmail.com>
2016-02-10 12:04:21 +0530

    Nested if statement program added

---
+++ 10_02_2016/max_between_three.sh
@@ -0,0 +1,19 @@
+echo "Program to find the max between three numbers:"
+echo "Number 1:"
+	read a
+echo "Number 2:"
+	read b
+echo "Number 3:"
+	read c
+
+if [ $a -gt $b ]
+then
+	let d=$a
+else
+	d=$b
+	if [ $d -lt $c ]
+	then
+		d=$c
+	fi
+fi
+echo The largest number is: $d
